378ca28e80113a880c5d86dfe1ec0edec2132ce2
masontmorris <masonmorris8@gmail.com>
2022-11-18 11:38:38 -0500

    basic functionality implemented, inquirer integration attempted and commented out

---
+++ app.js
@@ -0,0 +1,52 @@
+import translate from "translate";
+import fs from "fs";
+import inquirer from "inquirer";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+// userInputs();
+
+// async function userInputs() {
+//     const prompts = await inquirer
+//         .prompt([
+//             {
+//                 type: "input",
+//                 name: "from",
+//                 message: "What language are you translating from?",
+//             },
+//             {
+//                 type: "input",
+//                 name: "to",
+//                 message: "What language are you translating to?",
+//             },
+//         ])
+//         .then((answers) => {
+//             translate.from = answers.from;
+//             translate.to = answers.to;
+//         });
+// }
+
+translate.from = "en";
+translate.to = "es";
+
+translate.key = process.env.GOOGLE_KEY;
+
+const input = JSON.parse(fs.readFileSync("input/input.json"));
+console.log(input);
+var count = 1;
+
+const outputObj = [];
+
+async function translateText() {
+    for (var key in input) {
+        const output = await translate(input[count].text, { from: translate.from, to: translate.to });
+        console.log(output);
+        outputObj[count] = output;
+        count++;
+    }
+    outputObj.splice(0, 1);
+    console.log(outputObj);
+    fs.writeFileSync("output/output.json", JSON.stringify(outputObj));
+}
+
+translateText();
